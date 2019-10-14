d1b114f9c8c8ad9104e4aefbe5dfb837a1f87c58
Barburat <38035810+Barburat@users.noreply.github.com>
2019-10-14 17:30:47 +0200

    Camera and character controller
    Not finished

---
+++ ShrinesOfElementia/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+
+    public float InputX;
+    public float InputZ;
+    public Vector3 desiredMoveDirection;
+    public bool blockRotationPlayer;
+    public float desiredRotationSpeed;
+    public Animator anim;
+    public float Speed;
+    public float allowPlayerRotation;
+    public Camera cam;
+    public CharacterController controller;
+    public bool isGrounded;
+
+    private float verticalVel;
+    private Vector3 moveVector;
+
+
+    void Start()
+    {
+        cam = Camera.main;
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        InputMagnitude();
+        isGrounded = controller.isGrounded;
+        if (isGrounded)
+        {
+            verticalVel -= 0;
+        }
+        else
+        {
+            verticalVel -= 2;
+        }
+
+        moveVector = new Vector3(0, verticalVel, 0);
+        controller.Move(moveVector);
+    }
+
+    void PlayerMoveAndRotation()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        Camera camera = Camera.main;
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        if(blockRotationPlayer == false)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
+        }
+    }
+
+    void InputMagnitude()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        anim.SetFloat("InputZ", InputZ, 0.0f, Time.deltaTime * 2f);
+        anim.SetFloat("InputX", InputX, 0.0f, Time.deltaTime * 2f);
+
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        if(Speed > allowPlayerRotation)
+        {
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+            PlayerMoveAndRotation();
+        }
+        else if(Speed < allowPlayerRotation)
+        {
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+        }
+    }
+}
