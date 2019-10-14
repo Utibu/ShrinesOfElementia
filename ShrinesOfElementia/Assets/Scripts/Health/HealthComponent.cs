bacae3731806600a36861a63003274e38f56ffe4
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-10-14 15:51:29 +0200

    HealthComponent
    Added my own scene and added a health component script.

---
+++ ShrinesOfElementia/Assets/Scripts/Health/HealthComponent.cs
@@ -0,0 +1,34 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [Header("UI reference")]
+    [SerializeField] private GameObject canvas;
+    private HealthBarController healthBarController;
+
+    [Header("Health attributes")]
+    [SerializeField] private int maxHealth;
+    public int MaxHealth
+    {
+        get => maxHealth;
+        set
+        {
+            maxHealth = value;
+            CurrentHealth = maxHealth;
+        }
+    }
+
+    private int currentHealth;
+    public int CurrentHealth
+    {
+        get => currentHealth;
+        set
+        {
+
+        }
+    }
+}
