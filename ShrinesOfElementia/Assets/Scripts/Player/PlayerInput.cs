edcfb2a6975d5c3b719e384ad57fd6b2cf399744
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-10-15 14:39:14 +0200

    Added PlayerInput.cs
    Also made some small changes to my scene.

---
+++ ShrinesOfElementia/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,15 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        player = Player.Instance;
+    }
+}
