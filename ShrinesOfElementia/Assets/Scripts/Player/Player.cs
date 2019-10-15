a2205a9e6c7a6214e61e1d1749527b44b369873b
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-10-15 11:00:16 +0200

    Player reference
    Added a static player reference that currently holds a reference to the players health component.

---
+++ ShrinesOfElementia/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿// Author: Bilal El Medkouri
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { private set; get; }
+    public HealthComponent Health { private set; get; }
+
+    private void Awake()
+    {
+        // Prevents multiple instances
+        if (Instance == null) { Instance = this; }
+        else { Debug.Log("Warning: multiple " + this + " in scene!"); }
+
+        Health = GetComponent<HealthComponent>();
+    }
+}
