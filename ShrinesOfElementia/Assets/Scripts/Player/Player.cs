99383636cb6f687e2cdbc18a33ab4c35d40fd1a0
BMedkouri <Bilal.Medkouri@gmail.com>
2019-10-16 13:16:44 +0200

    Player Light Attack
    - Created an avatar mask for the upper body. This way, attack animation only affects the upper body, leaving the lower body as is.
    
    - Added an animation for the players basic attack.
    
    - The player can now attack by clicking on the left mouse button.

--- ShrinesOfElementia/Assets/Scripts/Player/Player.cs
+++ ShrinesOfElementia/Assets/Scripts/Player/Player.cs
@@ -11,15 +11,10 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         // Prevents multiple instances
-        if (Instance == null) { Instance = this; Debug.Log("Player Instance set to " + this); }
+        if (Instance == null) { Instance = this; }
         else { Debug.Log("Warning: multiple " + this + " in scene!"); }
 
-        Debug.Log("Health before getter: " + Health);
         Health = GetComponent<HealthComponent>();
-        Debug.Log("Health after: " + Health);
-
-        Debug.Log("Animator before getter: " + Animator);
         Animator = GetComponent<Animator>();
-        Debug.Log("Animator after: " + Animator);
     }
 }
--- ShrinesOfElementia/Assets/Scripts/Player/PlayerInput.cs
+++ ShrinesOfElementia/Assets/Scripts/Player/PlayerInput.cs
@@ -6,6 +6,8 @@ public class PlayerInput : MonoBehaviour
 {
     private Player player;
 
+    private float lightAttackTimer = 0f; // Temporary fix
+
     private void Start()
     {
         player = Player.Instance;
@@ -23,6 +25,34 @@ public class PlayerInput : MonoBehaviour
         {
             // Heals the player for 10 hp
             player.Health.CurrentHealth += 10;
+        }
+
+
+        // Mouse buttons, 0 - Primary Button, 1 - Secondary Button, 2 - Middle Click
+
+        // Left click / Primary button
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Temporary fix to stop animations from repeating
+            if (lightAttackTimer <= 0f)
+            {
+                lightAttackTimer = 1.5f;
+                player.Animator.SetTrigger("LightAttack1");
+            }
+        }
+
+        lightAttackTimer -= Time.deltaTime;
+
+        // Holding down left click / Primary button
+        if (Input.GetMouseButton(0))
+        {
+            // Heavy Attack
+        }
+
+        // Holding down right click / Secondary button
+        if (Input.GetMouseButton(1))
+        {
+            // Block
         }
     }
 }
