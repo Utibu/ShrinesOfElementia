72c4589586635bd317e754fc9feb190fadae829a
BMedkouri <Bilal.Medkouri@gmail.com>
2019-10-16 11:30:45 +0200

    PlayerInput
    - Removed controller test stuff folder, as it is not being used anymore.
    
    - Mapped two inputs for handling the players health. K deals 10 damage, and L heals for 10 hp.

--- ShrinesOfElementia/Assets/Scripts/Player/Player.cs
+++ ShrinesOfElementia/Assets/Scripts/Player/Player.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public static Player Instance { private set; get; }
-    public HealthComponent Health { private set; get; }
-    public Animator Animator { private set; get; }
+    public static Player Instance { get; private set; }
+    public HealthComponent Health { get; private set; }
+    public Animator Animator { get; private set; }
 
     private void Awake()
     {
         // Prevents multiple instances
-        if (Instance == null) { Instance = this; }
+        if (Instance == null) { Instance = this; Debug.Log("Player Instance set to " + this); }
         else { Debug.Log("Warning: multiple " + this + " in scene!"); }
 
+        Debug.Log("Health before getter: " + Health);
         Health = GetComponent<HealthComponent>();
+        Debug.Log("Health after: " + Health);
+
+        Debug.Log("Animator before getter: " + Animator);
         Animator = GetComponent<Animator>();
+        Debug.Log("Animator after: " + Animator);
     }
 }
--- ShrinesOfElementia/Assets/Scripts/Player/PlayerInput.cs
+++ ShrinesOfElementia/Assets/Scripts/Player/PlayerInput.cs
@@ -1,15 +1,28 @@
 ﻿// Author: Bilal El Medkouri
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
     private Player player;
 
-    private void Awake()
+    private void Start()
     {
         player = Player.Instance;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            // Deals 10 damage to the player
+            player.Health.CurrentHealth -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            // Heals the player for 10 hp
+            player.Health.CurrentHealth += 10;
+        }
     }
 }
