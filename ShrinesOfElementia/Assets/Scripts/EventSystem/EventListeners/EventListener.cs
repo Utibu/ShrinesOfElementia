d1e0c6e0484f9f4c57dacd89519b2381a0d70446
BMedkouri <Bilal.Medkouri@gmail.com>
2019-10-16 17:42:56 +0200

    EventSystem
    - Created an event system with Event.cs and EventListener.cs as superclasses for future events and listeners

---
+++ ShrinesOfElementia/Assets/Scripts/EventSystem/EventListeners/EventListener.cs
@@ -0,0 +1,18 @@
+﻿// Author: Bilal El Medkouri
+
+using UnityEngine;
+
+public abstract class EventListener<T> : MonoBehaviour where T : Event<T>
+{
+    protected virtual void Start()
+    {
+        Event<T>.RegisterListener(OnEvent);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        Event<T>.UnregisterListener(OnEvent);
+    }
+
+    protected virtual void OnEvent(T eventType) { }
+}
---
+++ ShrinesOfElementia/Assets/Scripts/EventSystem/Events/Event.cs
@@ -0,0 +1,35 @@
+﻿// Author: Bilal El Medkouri
+
+using UnityEngine;
+
+public abstract class Event<T> where T : Event<T>
+{
+    private bool hasFired;
+
+    public delegate void EventListener(T info);
+    public static event EventListener Listeners;
+
+    public static void RegisterListener(EventListener listener)
+    {
+        Listeners += listener;
+    }
+
+    public static void UnregisterListener(EventListener listener)
+    {
+        Listeners -= listener;
+    }
+
+    public void FireEvent()
+    {
+        if (hasFired)
+        {
+            throw new System.Exception("This event has already fired, to prevent infinite loops, you can't refire an event.");
+        }
+
+        hasFired = true;
+
+        Listeners?.Invoke(this as T);
+
+        Debug.Log(this + " has fired");
+    }
+}
