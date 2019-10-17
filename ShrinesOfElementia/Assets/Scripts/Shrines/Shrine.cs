a15c4f457e30042a1026edd22b99f8d0781619f4
Barburat <38035810+Barburat@users.noreply.github.com>
2019-10-17 12:36:11 +0200

    stuff

---
+++ ShrinesOfElementia/Assets/Scripts/Shrines/Shrine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrine : MonoBehaviour
+{
+    private ShrineAbility shrineAbility;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
