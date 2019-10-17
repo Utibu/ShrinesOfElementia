8d54b4309fe2c12f57fd02cf6e7c21b9e0728cc4
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-10-17 11:03:56 +0200

    Sword Collision
    - Made a script that handles the functionality of the players basic attack

---
+++ ShrinesOfElementia/Assets/Scripts/__PlayerSword/Sword.cs
@@ -0,0 +1,36 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Vector3 hitPoint;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position + (transform.forward * 1f), -transform.forward, out hit, 8, layerMask))
+        {
+            print("hit");
+
+            hitPoint = hit.point;
+
+            // Deal damage
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Ray ray = new Ray(transform.position + (transform.forward * 1f), -transform.forward);
+        Gizmos.DrawRay(ray);
+
+        Gizmos.DrawSphere(hitPoint, .2f);
+    }
+}
