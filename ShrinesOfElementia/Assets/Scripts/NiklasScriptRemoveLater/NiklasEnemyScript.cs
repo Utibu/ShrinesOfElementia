fe8de28548cc74ea3357306ca1c7c49b45eab156
Niklas Almqvist <git@utibu.se>
2019-10-18 13:35:45 +0200

    Enemy attack animation now triggable by code

---
+++ ShrinesOfElementia/Assets/Scripts/NiklasScriptRemoveLater/NiklasEnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NiklasEnemyScript : MonoBehaviour
+{
+    [SerializeField] private Transform enemy;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            enemy.GetComponent<Animator>().SetTrigger("ShouldAttack");
+        }
+    }
+}
