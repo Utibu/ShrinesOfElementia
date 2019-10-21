a020eb86d6d42203b29acec6f53df102013cb9b9
Barburat <38035810+Barburat@users.noreply.github.com>
2019-10-21 11:54:45 +0200

    Start of player statemachine (very poop)

---
+++ ShrinesOfElementia/Assets/Scripts/Player/PlayerStatemachine/PlayerStates/PlayerBaseState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseState : State
+{
+    protected PlayerStateMachine owner;
+
+
+    public override void Initialize(StateMachine stateMachine)
+    {
+        owner = (PlayerStateMachine)stateMachine;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
