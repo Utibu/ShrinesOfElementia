f9a3c84f45399bd4252afa6bdf1acba716b104e5
Niklas Almqvist <git@utibu.se>
2019-10-25 14:13:36 +0200

    Sounds added on actions

--- ShrinesOfElementia/Assets/PlayerSoundController.cs
+++ ShrinesOfElementia/Assets/PlayerSoundController.cs
@@ -6,10 +6,35 @@ public class PlayerSoundController : MonoBehaviour
 {
 
     [SerializeField] private AudioSource playerAudioSource;
+    [SerializeField] private AudioSource footAudioSource;
     [SerializeField] private AudioClip slashClip;
+    [SerializeField] private AudioClip swordHitClip;
+    [SerializeField] private AudioClip enemyHitClip;
+    [SerializeField] private AudioClip footstepClip;
+
+    public void Start()
+    {
+        EventSystem.Current.RegisterListener<DamageEvent>(PlayHitClip);
+    }
     public void PlaySlashClip()
     {
         playerAudioSource.PlayOneShot(slashClip);
+    }
+
+    public void PlayHitClip(DamageEvent ev)
+    {
+        if(ev.TargetGameObject.tag.Equals("Enemy"))
+        {
+            playerAudioSource.PlayOneShot(swordHitClip);
+        } else
+        {
+            playerAudioSource.PlayOneShot(enemyHitClip);
+        }
+    }
+
+    public void PlayFootstep()
+    {
+        footAudioSource.PlayOneShot(footstepClip);
     }
 
 }
