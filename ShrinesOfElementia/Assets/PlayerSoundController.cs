263910f9ad0a4c016558fb21139608fa457d2dfc
Niklas Almqvist <git@utibu.se>
2019-10-25 12:26:25 +0200

    Sounds added and slash sound works

---
+++ ShrinesOfElementia/Assets/PlayerSoundController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource playerAudioSource;
+    [SerializeField] private AudioClip slashClip;
+    public void PlaySlashClip()
+    {
+        playerAudioSource.PlayOneShot(slashClip);
+    }
+
+}
