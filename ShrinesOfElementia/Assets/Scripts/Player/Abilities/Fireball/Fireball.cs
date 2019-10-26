a129c68c5b4dfb5e70909e756e5e13dc092b134b
BMedkouri <Bilal.Medkouri@gmail.com>
2019-10-26 10:41:39 +0200

    Fireball
    - Added AoE-effects
    - Added a max range

--- ShrinesOfElementia/Assets/Scripts/Player/Abilities/Fireball/Fireball.cs
+++ ShrinesOfElementia/Assets/Scripts/Player/Abilities/Fireball/Fireball.cs
@@ -1,7 +1,5 @@
 ﻿// Author: Bilal El Medkouri
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -10,18 +8,67 @@ using UnityEngine;
 public class Fireball : MonoBehaviour
 {
     [Header("Temporary attributes")]
-    [SerializeField] private int damage;
+    [SerializeField] private int directHitDamage;
+    [SerializeField] private int aoeDamage;
+    [SerializeField] private float aoeRadius, maxRange;
+
+    private Vector3 spawnPosition, currentPosition;
+
+    private bool hasDealtDamage;
+
+    private void Awake()
+    {
+        spawnPosition = gameObject.transform.position;
+        currentPosition = spawnPosition;
+
+        hasDealtDamage = false;
+    }
+    private void Update()
+    {
+        currentPosition = gameObject.transform.position;
+
+        print(Vector3.Distance(spawnPosition, currentPosition));
+
+        if (Vector3.Distance(spawnPosition, currentPosition) >= maxRange)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (hasDealtDamage == true)
+            return;
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            DamageEvent damageEvent = new DamageEvent(gameObject + " has dealt " + damage + " damage to " + collision.gameObject, damage, gameObject, collision.gameObject);
-            EventSystem.Current.FireEvent(damageEvent);
+            DealDamage(collision.gameObject, directHitDamage);
+
+            hasDealtDamage = true;
         }
 
-        // Add AoE-effect here
+        AreaOfEffect();
 
         Destroy(gameObject);
+    }
+
+    private void AreaOfEffect()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, aoeRadius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].gameObject.CompareTag("Enemy"))
+            {
+                DealDamage(hitColliders[i].gameObject, aoeDamage);
+            }
+            i++;
+        }
+    }
+
+    private void DealDamage(GameObject damagedGameObject, int damage)
+    {
+        DamageEvent damageEvent = new DamageEvent(damagedGameObject + " has dealt " + damage + " damage to " + damagedGameObject, damage, gameObject, damagedGameObject);
+        EventSystem.Current.FireEvent(damageEvent);
     }
 }
