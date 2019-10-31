0c2ae5cfdcd413e46b7b4e630259de3117c63651
Trollsnor <sofiachylekauko@gmail.com>
2019-10-31 17:44:52 +0100

    im done

--- ShrinesOfElementia/Assets/Scripts/EnemyBehaviour/BasicEnemyStates/BasicEnemyBaseState.cs
+++ ShrinesOfElementia/Assets/Scripts/EnemyBehaviour/BasicEnemyStates/BasicEnemyBaseState.cs
@@ -12,7 +12,7 @@ public class BasicEnemyBaseState : State
 
     protected float distanceToPlayer;
     protected float attackRange = 1.3f;
-    protected float sightRange = 25.0f;
+    protected float sightRange = 18.0f;
     protected float castRange = 16f;
 
 
