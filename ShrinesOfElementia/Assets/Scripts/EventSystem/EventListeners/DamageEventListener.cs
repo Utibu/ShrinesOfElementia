fc7c32992263dccdbdd625c07fa56ceac3ad1913
Niklas Almqvist <git@utibu.se>
2019-10-31 16:19:13 +0100

    Knockback gone

--- ShrinesOfElementia/Assets/Scripts/EventSystem/EventListeners/DamageEventListener.cs
+++ ShrinesOfElementia/Assets/Scripts/EventSystem/EventListeners/DamageEventListener.cs
@@ -16,7 +16,7 @@ public class DamageEventListener : MonoBehaviour
 
         //pushback
         //damageEvent.TargetGameObject.GetComponent<EnemySM>().OnAttacked(damageEvent);
-        damageEvent.TargetGameObject.transform.position += damageEvent.TargetGameObject.transform.forward * -0.4f;
+        //damageEvent.TargetGameObject.transform.position += damageEvent.TargetGameObject.transform.forward * -0.4f;
 
         print(damageEvent.InstigatorGameObject + " has dealt " + damageEvent.Damage + " damage to " + damageEvent.TargetGameObject);
     }
