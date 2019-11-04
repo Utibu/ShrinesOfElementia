f7cab4afbf4c3c4a2fb78e1efdfa825800169ec6
Barburat <38035810+Barburat@users.noreply.github.com>
2019-11-04 12:54:46 +0100

    Start of combat manager

---
+++ ShrinesOfElementia/Assets/Scripts/Player/CombatManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    private MovementInput movementInput;
+    private bool inCombat;
+    private int enemiesAttacking;
+
+
+    private void Start()
+    {
+        movementInput = GetComponent<MovementInput>();
+        EventSystem.Current.RegisterListener<CombatEvent>(OnCombatEvent);
+        enemiesAttacking = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            print(enemiesAttacking);
+        }
+    }
+
+    private void OnCombatEvent(CombatEvent eve)
+    {
+        if(inCombat == false)
+        {
+            EnterCombat();
+        }
+        if (eve.EnteredCombat)
+        {
+            enemiesAttacking++;
+        }
+        else
+        {
+            enemiesAttacking--;
+        }
+
+        if(enemiesAttacking <= 0)
+        {
+            LeaveCombat();
+        }
+    }
+
+    private void EnterCombat()
+    {
+        Player.Instance.Animator.SetBool("InCombat", true);
+    }
+
+    private void LeaveCombat()
+    {
+        Player.Instance.Animator.SetBool("InCombat", false);
+    }
+}
