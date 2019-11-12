6499575fb1c9baa29a958df9989367dfd9659159
BMedkouri <Bilal.Medkouri@gmail.com>
2019-11-12 16:40:32 +0100

    Changes to Giant
    - Added a HP bar that is displayed when the boss fight is triggered.
    - Made the giant slightly slower by slowing down the animation speed to 0.7 (from 1.0)

---
+++ ShrinesOfElementia/Assets/Scripts/UI/BossHealthBarHandler.cs
@@ -0,0 +1,29 @@
+﻿// Author: Bilal El Medkouri
+
+using UnityEngine;
+
+public class BossHealthBarHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject bossHealthBar;
+
+    private void Awake()
+    {
+        bossHealthBar.SetActive(false);
+    }
+    private void Start()
+    {
+        BossEvents.Instance.OnBossFightAreaTriggerEnter += OnBossAreaEnter;
+        print(gameObject + " subscribed to BossFightAreaTriggerEnter");
+    }
+
+    private void OnBossAreaEnter()
+    {
+        bossHealthBar.SetActive(true);
+        BossEvents.Instance.OnBossFightAreaTriggerEnter -= OnBossAreaEnter;
+    }
+
+    private void OnDestroy()
+    {
+        BossEvents.Instance.OnBossFightAreaTriggerEnter -= OnBossAreaEnter;
+    }
+}
