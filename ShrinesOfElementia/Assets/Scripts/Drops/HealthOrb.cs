3326e5e9f884507bc3f704caedcf14839720c0a2
Barburat <38035810+Barburat@users.noreply.github.com>
2019-11-12 12:45:43 +0100

    Start of healthorb

---
+++ ShrinesOfElementia/Assets/Scripts/Drops/HealthOrb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthOrb : DroppableObject
+{
+    [SerializeField] int healthAmount;
+
+    protected override void Start()
+    {
+        
+    }
+
+    protected override void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player.Instance.Health.CurrentHealth += healthAmount;
+        }
+
+        Destroy(gameObject);
+    }
+}
