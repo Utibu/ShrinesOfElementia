db9122921f2451b4047c588af8d52fae4b4a356e
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-11-14 20:49:35 +0100

    Giant

---
+++ ShrinesOfElementia/Assets/Scripts/Actors/Enemies/Giants/__Attacks/GiantAttackParticleCollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantAttackParticleCollisionHandler : MonoBehaviour
+{
+    // Collides twice at the moment, fix this later
+    // TODO
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DamageEvent damageEvent = new DamageEvent(gameObject + " has dealt " + Giant.Instance.StompDamage + " damage to " + other, Giant.Instance.StompDamage, gameObject, other);
+            EventManager.Current.FireEvent(damageEvent);
+        }
+    }
+}
