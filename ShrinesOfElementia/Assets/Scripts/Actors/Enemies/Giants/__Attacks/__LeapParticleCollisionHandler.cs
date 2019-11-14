dc5e95b0f13da2b9fa76fca1ce84ddc34bd9b94f
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-11-14 21:17:13 +0100

    Giant - Colliders and Leap Damage
    - Changed some of the giants colliders
    - Added a particle system to leap. It also deals damage now.

---
+++ ShrinesOfElementia/Assets/Scripts/Actors/Enemies/Giants/__Attacks/__LeapParticleCollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class __LeapParticleCollisionHandler : MonoBehaviour
+{
+    // Collides twice at the moment, fix this later
+    // TODO
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DamageEvent damageEvent = new DamageEvent(gameObject + " has dealt " + Giant.Instance.LeapDamage + " damage to " + other, Giant.Instance.LeapDamage, gameObject, other);
+            EventManager.Current.FireEvent(damageEvent);
+        }
+    }
+}
--- ShrinesOfElementia/Assets/Scripts/ParticleControllers/GiantParticleEmitter.cs
+++ ShrinesOfElementia/Assets/Scripts/ParticleControllers/GiantParticleEmitter.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 public class GiantParticleEmitter : MonoBehaviour
 {
     [SerializeField] private ParticleSystem shockwaveParticleSystem;
+    [SerializeField] private ParticleSystem leapParticleSystem;
 
     public void FireShockwaveParticleSystem()
     {
         shockwaveParticleSystem.Play();
+    }
+
+    public void FireLeapParticleSystem()
+    {
+        leapParticleSystem.Play();
     }
 }
