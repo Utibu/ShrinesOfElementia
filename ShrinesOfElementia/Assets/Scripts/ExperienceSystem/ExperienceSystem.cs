de4a7bd0d970fc6c560cad91ff4ac7963e85e937
Bilal El Medkouri <bilal.medkouri@gmail.com>
2019-11-21 11:03:21 +0100

    Experience System
    - Started working on the experience system. Fleshed out the basic framework and will put it on hold for now, as I'm planning on starting with the player's state machine.

---
+++ ShrinesOfElementia/Assets/Scripts/ExperienceSystem/ExperienceSystem.cs
@@ -0,0 +1,68 @@
+﻿// Author: Bilal El Medkouri
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceSystem : MonoBehaviour
+{
+    [Header("Experience")]
+    [SerializeField] private int currentExperience;
+    [SerializeField] private int maxExperience;
+
+    [Header("Level")]
+    [SerializeField] private int currentLevel;
+    [SerializeField] private int maxLevel;
+
+    // Bygg ut alla properties så att de faktiskt gör något vid get och set. Åtminstone set!
+    public int CurrentExperience
+    {
+        get => currentExperience;
+        set
+        {
+            // Gör mer här
+            currentExperience = value;
+        }
+    }
+    public int MaxExperience
+    {
+        get => maxExperience;
+        set
+        {
+            // Gör mer här
+            maxExperience = value;
+        }
+    }
+    public int CurrentLevel
+    {
+        get => currentLevel;
+        set
+        {
+            // Gör mer här
+            currentLevel = value;
+        }
+    }
+
+    private void Awake()
+    {
+        CurrentExperience = 0;
+
+        CurrentLevel = 1;
+
+        MaxExperience = CalculateMaxExperience();
+    }
+
+    private int CalculateMaxExperience()
+    {
+        int newMaxExperience = 10 * CurrentLevel * CurrentLevel + 90;
+        return newMaxExperience;
+    }
+
+    /* Byt till denna vid senare stadie, när vi har fått exp-values på fiender att scale:a med CurrentLevel
+    private int CalculateMaxExperience()
+    {
+        int newMaxExperience = 10 * CurrentLevel * CurrentLevel + 90 * CurrentLevel;
+        return newMaxExperience;
+    }
+    */
+}
