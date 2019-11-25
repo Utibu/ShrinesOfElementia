7dab051277129a253f221b5ff072bb956e362721
Barburat <38035810+Barburat@users.noreply.github.com>
2019-11-25 16:47:46 +0100

    more indicator stuff

--- ShrinesOfElementia/Assets/Scripts/UI/AbilityIndicator.cs
+++ ShrinesOfElementia/Assets/Scripts/UI/AbilityIndicator.cs
@@ -32,9 +32,17 @@ public class AbilityIndicator : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(CameraReference.Instance.transform.position, CameraReference.Instance.transform.forward, out hit, indicatorRange, layerMask))
         {
-            print(Quaternion.Euler(hit.normal));
-            indicator.transform.position = new Vector3(hit.point.x, hit.point.y + 0.01f, hit.point.z);
-            indicator.transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            Quaternion rayRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            if(rayRotation.eulerAngles.x < 30f || rayRotation.eulerAngles.x > -30f || rayRotation.eulerAngles.z < 30f || rayRotation.eulerAngles.z > -30f)
+            {
+                indicator.transform.position = new Vector3(hit.point.x, hit.point.y + 0.01f, hit.point.z);
+                indicator.transform.rotation = rayRotation;
+                print(rayRotation.eulerAngles);
+            }
+            else
+            {
+                print("angle too steep");
+            }
         }
     }
 
