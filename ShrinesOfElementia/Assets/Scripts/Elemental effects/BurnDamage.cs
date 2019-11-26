d3a846375dbe173a0896935396c0137f7fe495b2
Trollsnor <sofiachylekauko@gmail.com>
2019-11-26 14:49:30 +0100

    boss deals burn damage

--- ShrinesOfElementia/Assets/Scripts/Elemental effects/BurnDamage.cs
+++ ShrinesOfElementia/Assets/Scripts/Elemental effects/BurnDamage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//Author: Sofia Kauko
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,16 @@ public class BurnDamage : MonoBehaviour
 {
 
     [SerializeField] private int DamagePerTick;
+    [SerializeField] private float DisableDuration;
     private float tickTime = 0.5f;
     private float countdown;
     private HealthComponent burnvictim;
+    private bool isActive = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        EventManager.Current.RegisterListener<GeyserCastEvent>(PauseBurn);
         countdown = tickTime;
     }
 
@@ -24,17 +28,42 @@ public class BurnDamage : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.TryGetComponent<HealthComponent>(out burnvictim))
+        if (other.TryGetComponent<HealthComponent>(out burnvictim) && isActive)
         {
-
             countdown -= Time.deltaTime;
-
             if(countdown < 0)
             {
                 burnvictim.CurrentHealth -= DamagePerTick;
+                Debug.Log("burndamage dealt");
                 countdown = tickTime;
             }
-            
+        }
+    }
+
+    private void PauseBurn(GeyserCastEvent ev)
+    {
+        if (Vector3.Distance(gameObject.transform.position, ev.affectedPosition) < ev.effectRange)
+        {
+            isActive = false;
+            GetComponent<Light>().enabled = false;
+            TimerManager.Current.SetNewTimer(gameObject, DisableDuration, EnableBurn);
+        }
+    }
+
+    private void EnableBurn()
+    {
+        isActive = true;
+        GetComponent<Light>().enabled = true;
+    }
+
+    private void OnDestroy()
+    {
+        try{
+            EventManager.Current.UnregisterListener<GeyserCastEvent>(PauseBurn);
+        }
+        catch (System.NullReferenceException nullRef)
+        {
+            Debug.Log("NullReferenceExeption caught: " + nullRef.Data);
         }
     }
 }
