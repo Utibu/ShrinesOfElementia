ba51faace99e3b4449a5556460009ece7f305d7f
Trollsnor <sofiachylekauko@gmail.com>
2019-11-27 15:36:50 +0100

    more forgiving stagger

--- ShrinesOfElementia/Assets/Scripts/Actors/Player/MovementInput.cs
+++ ShrinesOfElementia/Assets/Scripts/Actors/Player/MovementInput.cs
@@ -387,13 +387,9 @@ public class MovementInput : MonoBehaviour
             Debug.Log("PLAYER IS STAGGERED");
             player.Animator.SetTrigger("OnStagger");
             isStaggered = true;
-            TimerManager.Current.SetNewTimer(gameObject, 0.9f, Recover);
+            TimerManager.Current.SetNewTimer(gameObject, 0.7f, Recover);
             animator.speed -= staggerAnimationSlow;
-            /*
-            //initialize timer prefab and set its variables. 
-            GameObject timer = Instantiate(timerObject, player.transform);
-            timer.GetComponent<Timer>().SetVariables(gameObject, 0.9f, Recover); // could be done with = new Timer(...) ?
-            */
+            
         }
         
         
