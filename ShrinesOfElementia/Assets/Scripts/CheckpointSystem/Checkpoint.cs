9b8105f96e68b029efbb853e2924fe4ada441563
Trollsnor <sofiachylekauko@gmail.com>
2019-12-15 20:53:22 +0100

    checkpoint prefab

--- ShrinesOfElementia/Assets/Scripts/CheckpointSystem/Checkpoint.cs
+++ ShrinesOfElementia/Assets/Scripts/CheckpointSystem/Checkpoint.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
+﻿//Author jocke?
+//co-Author: Sofia Kauko
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    //[SerializeField] private GameObject spawnLocation; wat?
+    [SerializeField]private GameObject spawnLocation;
+    private Collider coll;
     private bool isActive;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider>();
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +23,7 @@ public class Checkpoint : MonoBehaviour
             Debug.Log("Checkpoint taken");
             isActive = true;
             EventManager.Instance.FireEvent(new CheckpointEvent("checkpoint activated", gameObject.transform.position));
+            coll.enabled = false; // just turn off collider so it wont check on trigger all the time. 
         }
     }
 }
