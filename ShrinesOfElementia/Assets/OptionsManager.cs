e44d261eea5d9a6edae00c75311dbc301e5e22f6
Jocke <jockeljung1@gmail.com>
2019-12-15 18:17:25 +0100

    Resolution, graphics, screen mode and antialiasing settings + menu changes

---
+++ ShrinesOfElementia/Assets/OptionsManager.cs
@@ -0,0 +1,72 @@
+﻿//Author: Joakim Ljung
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsManager : MonoBehaviour
+{
+
+    Resolution[] resolutions;
+    //[SerializeField] private Dropdown resolutionDropdown;
+    [SerializeField]private TMP_Dropdown resolutionDropdown;
+
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetScreenMode(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetAntiAliasing(int aliasingIndex)
+    {
+        switch (aliasingIndex)
+        {
+            case 0:
+                QualitySettings.antiAliasing = 0;
+                break;
+            case 1:
+                QualitySettings.antiAliasing = 2;
+                break;
+            case 2:
+                QualitySettings.antiAliasing = 4;
+                break;
+            case 3:
+                QualitySettings.antiAliasing = 8;
+                break;
+        }
+    }
+}
