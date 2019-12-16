76f9c98ac06f18d9043dfe2b5b2984197d87d700
Niklas Almqvist <git@utibu.se>
2019-12-16 09:40:50 +0100

    Buildscene

--- ShrinesOfElementia/Assets/Scripts/Actors/Player/InGameMenuController.cs
+++ ShrinesOfElementia/Assets/Scripts/Actors/Player/InGameMenuController.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class InGameMenuController : MonoBehaviour
 {
 
-    [SerializeField]private GameObject menuPanel;
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject soundPanel;
 
 
     private void Update()
@@ -29,6 +30,18 @@ public class InGameMenuController : MonoBehaviour
     {
         GameManager.Instance.Save();
         SceneManager.LoadScene(0);
+    }
+
+    public void OnSoundOptionsClick()
+    {
+        menuPanel.SetActive(false);
+        soundPanel.SetActive(true);
+    }
+
+    public void OnBackClick()
+    {
+        menuPanel.SetActive(true);
+        soundPanel.SetActive(false);
     }
 
     public void OnResumeGame()
