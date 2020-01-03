fc97662fd7e5f31d8a62193ad0d866e28e97b081
Meti ☆ <MetieSweetie@gmail.com>
2020-01-03 14:13:35 +0100

    Level up Particles

---
+++ ShrinesOfElementia/Assets/Scripts/UI/LevelUp.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelUp : MonoBehaviour
+{
+    [Header("Inputs")]
+    //[SerializeField] public GameObject self;
+    [SerializeField] public ExperienceSystem playerEXP;
+    [SerializeField] public float levelnum;
+    [SerializeField] public Image tab;
+    [SerializeField] public Image border;
+    [SerializeField] public Image xp;
+    [SerializeField] public TextMeshProUGUI lvlText;
+    [SerializeField] public TextMeshProUGUI levelUpText;
+
+    [Header("Bools")]
+    [SerializeField] public bool beginXpScroll = false;
+
+    [Header("enums")]
+    [SerializeField] public State state;
+
+    [Header("fideleties")]
+    [SerializeField] public float tabspeed = 0.02f;
+    [SerializeField] public float lvlTextSpeed = 0.02f;
+    [SerializeField] public float textRollSpeed = 0.2f;
+    [SerializeField] public float fillSpeed = 0.5f;
+    [SerializeField] public float rollMagnitude = 0.02f;
+
+    [Header("Inputs")]
+    [SerializeField] public TimerVariant levelnumAppear;
+    [SerializeField] public TimerVariant dissapearAll;
+
+    [SerializeField] private float progress = 0f;
+    [SerializeField] private GameObject shine;
+    public bool rollLevelbool = false;
+
+    [SerializeField] public enum State
+    {
+        waiting,
+        lvlUp,
+        levelNum,
+        dissapearAll,
+        resetAll, //resets all states.
+    }
+
+    private void Start()
+    {
+        EventManager.Instance.RegisterListener<LevelUpEvent>(LevelUpCall);
+        shine.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        levelnum = playerEXP.CurrentLevel;
+        setInvisible();
+        
+    }
+
+    void setInvisible()
+    {
+
+    }
+
+
+    public void LevelUpCall(LevelUpEvent ev)
+    {
+        state = State.resetAll;
+        levelnum = playerEXP.CurrentLevel;
+    }
+
+
+    void UiUpdate()
+    {
+        switch (state)
+        {
+            //initial state which does nothing.
+            case State.waiting:
+                break;
+
+            //initially levels up.
+            case State.lvlUp:
+                levelnum = playerEXP.CurrentLevel;
+                fadeInTab();
+
+                //set count!
+                lvlText.text = "0";
+
+                //start timers...!
+                levelnumAppear.finished = false;
+                shine.SetActive(true);
+
+                break;
+            case State.levelNum:
+                shine.SetActive(false);
+                lvlText.alpha = 255;
+                levelCountRise();
+                appearLevelUpTxt();
+                fadeInBorder();
+                rollXP();
+                //display particle
+                //bool rollLevelbool true
+                break;
+            //fade out all
+            case State.dissapearAll:
+                lvlText.alpha = 0;
+
+                rollLevelbool = false;
+                hide();
+                shine.SetActive(false);
+
+                //state = State.waiting;
+                break;
+
+            //resets all to 0 state before starting Level up Instance. This stops potential glitches where the player leveling up many times at once causes bizzare events.
+            case State.resetAll:
+                var tabCol = tab.color;
+                tabCol.a = 0;
+                tab.color = tabCol;
+                border.color = tabCol;
+                rollLevelbool = false;
+                lvlText.alpha = 0;
+                levelUpText.alpha = 0;
+                progress = 0f;
+                shine.SetActive(false);
+                xp.fillAmount = 0;
+                levelnum = playerEXP.CurrentLevel;
+                state = State.lvlUp;
+                levelnumAppear.finished = true;
+                dissapearAll.finished = true;
+                break;
+        }
+
+        //Debug remove if finished.
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            state = State.resetAll;
+        }
+    }
+
+    public void fadeInTab()
+    {
+        var tabCol = tab.color;
+        if(tabCol.a <= 1)
+        {
+            tabCol.a += tabspeed;
+            tab.color = tabCol;
+        } else
+        {
+            
+            state = State.levelNum;
+        }
+    }
+
+    public void fadeInBorder()
+    {
+        var tabCol = border.color;
+        if (tabCol.a <= 1)
+        {
+            tabCol.a += tabspeed;
+            border.color = tabCol;
+        }
+    }
+
+    void rollXP()
+    {
+        if(xp.fillAmount < 1)
+        {
+            xp.fillAmount += fillSpeed*rollMagnitude;
+        }
+    }
+
+    public void callNewState()
+    {
+        state = State.levelNum;
+    }
+
+    public void appearLevelTxt()
+    {
+        
+        if (lvlText.alpha <= 255)
+        {
+            lvlText.alpha += lvlTextSpeed;
+        }
+    }
+    public void appearLevelUpTxt()
+    {
+        if (levelUpText.alpha <= 255)
+        {
+            levelUpText.alpha += lvlTextSpeed;
+        }
+    }
+
+    public void levelCountRise()
+    {
+        if(progress < levelnum)
+        {
+            progress += textRollSpeed;
+            lvlText.text = ((int)progress).ToString();
+        } else
+        {
+            if (!rollLevelbool)
+            {
+                dissapearAll.finished = false; //begin counter until fadeout
+                rollLevelbool = true;
+            } 
+        }
+    }
+
+    public void dissapear() { state = State.dissapearAll; }
+
+    public void hide()
+    {
+        //levelUpText
+        
+        //LevelCount
+        if (levelUpText.alpha > 0)
+        {
+            levelUpText.alpha -= lvlTextSpeed*2.5f;
+        }
+
+        //hidetab
+        var tabCol = tab.color;
+        if (tabCol.a >= 0)
+        {
+            tabCol.a -= tabspeed;
+            tab.color = tabCol;
+        }
+
+        //hidetab
+        var bordervar = border.color;
+        if (bordervar.a >= 0)
+        {
+            bordervar.a -= tabspeed;
+            border.color = bordervar;
+        }
+
+        ////xp
+        if (xp.fillAmount >= 0)
+        {
+            xp.fillAmount -= fillSpeed;
+        }
+    }
+
+    public void hideLvlNum()
+    {
+
+    }
+
+    //Girder Tab appears at same (time) as Tab Appear
+    //Tab Appear(time) -> Level num show(time)
+    //          -> while level num opacitying in Xp bar quickscroll up (scroll)  (Optional if time)
+    //Tab appear 0.3 msec -> Level Up show!
+    //                      Level Up Resizes up + down + play animation for it (Optional)
+    //Level increase perhaps glow (Optional if time)
+
+
+    private void Update() { UiUpdate(); }
+}
---
+++ ShrinesOfElementia/Assets/Shaders/PhotoshopLighten.shader
@@ -0,0 +1,73 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/PhotoshopLighten"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "" {}
+		_Color("Blend Color", Color) = (0.5, 0.5, 0.5, 1.0)
+	}
+
+		SubShader
+		{
+			Tags
+			{
+				"Queue" = "Transparent"
+				"RenderType" = "Transparent"
+			}
+
+			Lighting Off
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#pragma fragmentoption ARB_precision_hint_fastest
+
+				#include "UnityCG.cginc"
+
+				struct appdata_custom
+				{
+					float4 vertex : POSITION;
+					fixed2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float4 vertex : POSITION;
+					fixed2 uv : TEXCOORD0;
+				};
+
+				sampler2D _MainTex;
+				fixed4 _MainTex_ST;
+				fixed4 _Color;
+
+				v2f vert(appdata_custom v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv,_MainTex);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : COLOR
+				{
+					fixed4 diffuse = tex2D(_MainTex, i.uv);
+					fixed luminance = dot(diffuse, fixed4(0.2126, 0.7152, 0.0722, 0));
+					fixed oldAlpha = diffuse.a;
+
+					if (luminance < 0.5)
+						diffuse *= 2 * _Color;
+					else
+						diffuse = 1 - 2 * (1 - diffuse) * (1 - _Color);
+
+					diffuse.a = oldAlpha * _Color.a;
+					return diffuse;
+				}
+				ENDCG
+			}
+		}
+			Fallback off
+}
