9c2cf2dc3eb53486062bb5196bd14939276ed3a0
Niklas Almqvist <git@utibu.se>
2020-01-03 10:17:12 +0100

    Enemy sounds

--- ShrinesOfElementia/Assets/Scripts/AudioControllers/EnemyAudioController.cs
+++ ShrinesOfElementia/Assets/Scripts/AudioControllers/EnemyAudioController.cs
@@ -7,6 +7,7 @@ public class EnemyAudioController : MonoBehaviour
     [SerializeField] private AudioClip spinClip;
     [SerializeField] private AudioClip dieClip;
     [SerializeField] private AudioClip castClip;
+    [SerializeField] private AudioClip meleeWindupClip;
     private AudioSource audioSource;
 
     public void Start()
@@ -37,6 +38,14 @@ public class EnemyAudioController : MonoBehaviour
         if (castClip != null && audioSource != null)
         {
             this.audioSource.PlayOneShot(castClip);
+        }
+    }
+
+    public void PlayMeleeWindup()
+    {
+        if (meleeWindupClip != null && audioSource != null)
+        {
+            this.audioSource.PlayOneShot(meleeWindupClip);
         }
     }
 }
--- ShrinesOfElementia/Assets/Scripts/AudioControllers/PlayerSoundController.cs
+++ ShrinesOfElementia/Assets/Scripts/AudioControllers/PlayerSoundController.cs
@@ -58,7 +58,6 @@ public class PlayerSoundController : MonoBehaviour
             {
                 if(ev.InflictedFromWater && waterHitClip != null)
                 {
-                    Debug.LogWarning("WAterhitclip?");
                     playerAudioSource.PlayOneShot(waterHitClip);
                 }
                 else if (ev.IsAbility)
