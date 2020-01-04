087da776d2dea05b5943444f721f876fc28f78ea
Niklas Almqvist <git@utibu.se>
2020-01-04 14:13:48 +0100

    Bosses now have all the sounds that are needed

--- ShrinesOfElementia/Assets/GiantAudioManager.cs
+++ ShrinesOfElementia/Assets/GiantAudioManager.cs
@@ -8,9 +8,15 @@ public class GiantAudioManager : MonoBehaviour
 {
 
     [SerializeField] private AudioSource footAudioSource;
+    [SerializeField] private AudioSource slashAudioSource;
+    [SerializeField] private AudioSource abilityAudioSource;
 
     [SerializeField] private AudioClip footstepClip;
     [SerializeField] private AudioClip shortFootstepClip;
+    [SerializeField] private AudioClip slashClip;
+    [SerializeField] private AudioClip chargeAbilityClip;
+    [SerializeField] private AudioClip shootAbilityClip;
+    [SerializeField] private AudioClip waveClip;
 
     private float footstepTimer = 0f;
     private float timeBeforeNextAllowedStep = 0.1f;
@@ -28,5 +34,37 @@ public class GiantAudioManager : MonoBehaviour
     public void PlayShortFootstep()
     {
         footAudioSource.PlayOneShot(shortFootstepClip);
+    }
+
+    public void PlaySlashClip()
+    {
+        if(slashAudioSource != null)
+        {
+            slashAudioSource.PlayOneShot(slashClip);
+        }
+    }
+
+    public void PlayChargeClip()
+    {
+        if (abilityAudioSource != null)
+        {
+            abilityAudioSource.PlayOneShot(chargeAbilityClip);
+        }
+    }
+
+    public void PlayShootClip()
+    {
+        if (abilityAudioSource != null)
+        {
+            abilityAudioSource.PlayOneShot(shootAbilityClip);
+        }
+    }
+
+    public void PlayWaveSound()
+    {
+        if (abilityAudioSource != null)
+        {
+            abilityAudioSource.PlayOneShot(waveClip);
+        }
     }
 }
