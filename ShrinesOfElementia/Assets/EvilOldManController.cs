d74448df361c95d89cb956e91ae03001de58ebc6
Trollsnor <sofiachylekauko@gmail.com>
2020-01-05 20:30:07 +0100

    old man at the end

---
+++ ShrinesOfElementia/Assets/EvilOldManController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilOldManController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.Instance.RegisterListener<BossDeathEvent>(OnBossDeath);
+    }
+    
+
+    private void OnBossDeath(BossDeathEvent ev)
+    {
+        if (ev.ElementType.Equals("Wind")) // bc its last boss.
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
