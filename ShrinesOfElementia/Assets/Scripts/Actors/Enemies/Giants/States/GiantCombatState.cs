f77109683d3944b4a5299f8a1d190fbfa0de59a5
Trollsnor <sofiachylekauko@gmail.com>
2020-01-06 11:50:39 +0100

    Less retarded boss turn
    it now "interpolerar" over time

--- ShrinesOfElementia/Assets/Scripts/Actors/Enemies/Giants/States/GiantCombatState.cs
+++ ShrinesOfElementia/Assets/Scripts/Actors/Enemies/Giants/States/GiantCombatState.cs
@@ -14,6 +14,11 @@ public class GiantCombatState : GiantBaseState
 
     public override void HandleUpdate()
     {
+        //turn towards palyer
+        Vector3 direction = (Player.Instance.transform.position - owner.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));    // flattens the vector3
+        owner.transform.rotation = Quaternion.Slerp(owner.transform.rotation, lookRotation, Time.deltaTime * 3f);
+
         if (owner.Animator.GetBool("IsAttacking") == false)
         {
             owner.Transition<GiantPhaseOneState>();
